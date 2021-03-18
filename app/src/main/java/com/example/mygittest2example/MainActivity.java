5d015e24d9ec6fc0e78982519e10141de183a0ae
jinsu-song <songjinsu486@naver.com>
2021-03-18 13:22:23 +0900

    activity_main 화면작업

--- app/src/main/java/com/example/mygittest2example/MainActivity.java
+++ app/src/main/java/com/example/mygittest2example/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.mygittest2example;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private TextView tvName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        tvName = findViewById(R.id.tvName);
     }
 }
